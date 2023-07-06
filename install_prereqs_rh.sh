f6c54e19859209a58d838f0b6c9e53b449585e5a
Todd S <18294602+seidelma@users.noreply.github.com>
2023-07-06 07:52:17 -0700

    Fix prereq installation scripts

--- install_prereqs_rh.sh
+++ install_prereqs_rh.sh
@@ -2,4 +2,4 @@
 set +h -exuo pipefail
 
 # Handle CentOS/Fedora prerequisites
-yum -y install gcc-c++ git python3-devel bison flex m4 autoconf automake make rpm-build texinfo help2man gettext-devel libtool rsync
+yum -y install gcc-c++ git python3-devel bison flex m4 autoconf automake make rpm-build texinfo help2man gettext-devel libtool rsync tmux
