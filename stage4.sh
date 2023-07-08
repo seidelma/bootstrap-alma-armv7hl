16672dd60934816ac0aeb0616a1025aed09a89af
Todd S <18294602+seidelma@users.noreply.github.com>
2023-07-08 12:44:12 -0700

    Fix stage4 build and manually add pyproject-rpm-macros for now

--- stage4.sh
+++ stage4.sh
@@ -10,33 +10,33 @@ else
     echo "The script config file $CONFIGRCFILE was not found, exiting"
     exit 1
 fi
-if [ -f "$STAGE3_PACKAGESRCFILE" ]; then
-    . "$STAGE3_PACKAGESRCFILE"
+if [ -f "$STAGE4_PACKAGESRCFILE" ]; then
+    . "$STAGE4_PACKAGESRCFILE"
 else
-    echo "The package config file $STAGE3_PACKAGESRCFILE was not found; exiting"
+    echo "The package config file $STAGE4_PACKAGESRCFILE was not found; exiting"
     exit 1
 fi
 
 if [ ! -f /.in_chroot ]; then
-	echo "Error: this script must be run from within the chroot environment. Do a:"
-	echo "    sudo chroot ${INSTALL_DIR} /bin/bash"
-	echo "And then rerun it"
-	exit 1
+    echo "Error: this script must be run from within the chroot environment. Do a:"
+    echo "    sudo chroot ${INSTALL_DIR} /bin/bash"
+    echo "And then rerun it"
+    exit 1
 fi
+
+STAGE_SPECFILE_DIR="${SCRIPT_DIR}/stage4/SPECS"
 
 pkg_sucs=0
 pkg_fail=0
 for pkg in $(python ${SCRIPT_DIR}/get_buildable_packages.py); do
-    if mock --rebuild $pkg; then
-        for file in $(find ${MOCK_RESULT_DIR} -name "*rpm" -a -not -name "*src.rpm"); do
-            cp -av "$file" "$STAGE4_REPO_DIR"
-        done
+    mock_build_opts="--additional-package=pyproject-rpm-macros"
+    if mock_build "$pkg"; then
         createrepo "$STAGE4_REPO_DIR"
-	let "pkg_sucs++"
-	echo "$pkg_sucs packages completed successfully"
+        let "pkg_sucs=$pkg_sucs+1"
+        echo "$pkg_sucs packages completed successfully"
     else
-        let "pkg_fail++"
-	echo "Building package $pkg failed; continuing"
+        let "pkg_fail=$pkg_fail+1"
+        echo "Building package $pkg failed; continuing"
     fi
     echo "$pkg_sucs successes, $pkg_fail failures so far"
 done
